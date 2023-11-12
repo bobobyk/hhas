8dd434ab12897ea8220c966509f40ea07d3a3a8a
bobobyk <mykha07@mail.ru>
2023-11-12 04:46:08 +0300

    sdfsdsdfsd
    sdffsdfsdf

---
+++ kk.py
@@ -0,0 +1,26 @@
+#C
+
+# import random
+
+# a = sorted(set([random.randint(0, 10) for a in range(6)]))
+# b = sorted(set([random.randint(0, 10) for b in range(6)]))
+# print(a)
+# print(b)
+
+# for i in a:
+#     for j in b:
+#         if i == j:
+#             print(i)
+
+#F
+
+# with open('input.txt', 'r') as ff:
+#     a = ff.read()
+
+# wor = a.split()
+
+# poh_wor = set(wor)
+
+# count  = len(poh_wor)
+
+# print(count)
